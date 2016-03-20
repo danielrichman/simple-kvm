d656976de0cbfb44adedfbb5862805180e336bdd
Daniel Richman <main@danielrichman.co.uk>
2016-03-20 17:35:11 +0000

    make it easier to extend guest-di-preseed

--- guest-di-preseed/build.sh
+++ guest-di-preseed/build.sh
@@ -5,5 +5,6 @@ dpkg-deb --build manual-partitioning
 mv manual-partitioning.deb extra/manual-partitioning.udeb
 cp /usr/lib/debian-installer/images/8/amd64/text/debian-installer/amd64/initrd.gz .
 gunzip initrd.gz 
-(cd extra; find . | fakeroot cpio --quiet -F ../initrd --append -o -H newc)
+(cd extra;  find . | fakeroot cpio --quiet -F ../initrd --append -o -H newc)
+(cd extra2; find . | fakeroot cpio --quiet -F ../initrd --append -o -H newc)
 gzip initrd 
