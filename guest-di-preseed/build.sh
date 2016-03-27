2d917b4dcd55f1545861ba0ea4c790bd17ba8f65
Daniel Richman <main@danielrichman.co.uk>
2016-03-27 13:06:11 +0100

    build.sh cpio follow symlinks

--- guest-di-preseed/build.sh
+++ guest-di-preseed/build.sh
@@ -5,6 +5,5 @@ dpkg-deb --build manual-partitioning
 mv manual-partitioning.deb extra/manual-partitioning.udeb
 cp /usr/lib/debian-installer/images/8/amd64/text/debian-installer/amd64/initrd.gz .
 gunzip initrd.gz 
-(cd extra;  find . | fakeroot cpio --quiet -F ../initrd --append -o -H newc)
-(cd extra2; find . | fakeroot cpio --quiet -F ../initrd --append -o -H newc)
-gzip initrd 
+(cd extra; find -L . | fakeroot cpio --quiet -LF ../initrd --append -o -H newc)
+gzip --fast initrd
--- host-di-preseed/build.sh
+++ host-di-preseed/build.sh
@@ -9,5 +9,5 @@ done
 
 cp /usr/lib/debian-installer/images/8/amd64/text/debian-installer/amd64/{linux,initrd.gz} .
 gunzip initrd.gz 
-(cd extra; find . | fakeroot cpio --quiet -F ../initrd --append -o -H newc)
-gzip initrd 
+(cd extra; find -L . | fakeroot cpio --quiet -LF ../initrd --append -o -H newc)
+gzip --fast initrd
