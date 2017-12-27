638dbfe7631c586a3c71b98e7adb1c57682b9786
Daniel Richman <main@danielrichman.co.uk>
2017-12-27 15:49:16 +0000

    add run-ansible-udeb

---
+++ run-ansible-udeb/build.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+set -o errexit -o nounset -o pipefail -o xtrace
+
+test -d DEBIAN
+mkdir -p .build
+rsync -av DEBIAN/ .build/DEBIAN
+dpkg-deb --build .build ../daniel-preseed-lib/run-ansible.udeb
