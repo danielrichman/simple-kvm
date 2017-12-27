7de496a9caa96379057dae1ff9a58f153e699372
Daniel Richman <main@danielrichman.co.uk>
2017-12-27 15:50:28 +0000

    run-ansible-udeb output path and gitignore

--- run-ansible-udeb/build.sh
+++ run-ansible-udeb/build.sh
@@ -4,4 +4,4 @@ set -o errexit -o nounset -o pipefail -o xtrace
 test -d DEBIAN
 mkdir -p .build
 rsync -av DEBIAN/ .build/DEBIAN
-dpkg-deb --build .build ../daniel-preseed-lib/run-ansible.udeb
+dpkg-deb --build .build ../preseed-lib/run-ansible.udeb
