32f17b4d15f47f5b37bfb9a9a07f26047259a36a
Daniel Richman <main@danielrichman.co.uk>
2017-12-28 01:47:02 +0000

    debconf fixes

--- run-ansible-udeb/debconf.py
+++ run-ansible-udeb/debconf.py
@@ -3,8 +3,7 @@ from __future__ import absolute_import, division, print_function
 import os
 import ansible.utils.display 
 from ansible.plugins.callback import CallbackBase
-
-display = ansible.utils.display.Display()
+from __main__ import display
 
 class CallbackModule(CallbackBase):
     CALLBACK_VERSION = 2.0
@@ -12,20 +11,20 @@ class CallbackModule(CallbackBase):
     CALLBACK_NAME = 'debconf'
 
     def __init__(self):
-        conn = None
+        self._enabled = False
 
         if "DEBCONF_REDIR" in os.environ:
             try:
-                conn = os.fdopen(3, 'w')
+                self._debconf_in  = os.fdopen(os.dup(0), 'r')
+                self._debconf_out = os.fdopen(3, 'w')
             except:
                 display.vvvv("failed to fdopen debconf fd")
             else:
+                self._enabled = True
                 display.vvvv("opened debconf fd")
         else:
             display.vvvv("debconf callbacks disabled as DEBCONF_REDIR unset")
 
-        self._debconf_connection = conn
-            
         # Might confuse child processes
         for env in ["DEBCONF_OLD_FD_BASE", "DEBCONF_REDIR", 
                     "DEBIAN_FRONTEND", "DEBIAN_HAS_FRONTEND"]:
@@ -35,18 +34,22 @@ class CallbackModule(CallbackBase):
                 pass
 
         super(CallbackModule, self).__init__()
+    
+    def _communicate(self, line):
+        if self._enabled:
+            display.vvvv("debconf_out: {}".format(line))
+            self._debconf_out.write(line)
+            self._debconf_out.flush()
+            reply = self._debconf_in.readline()
+            display.vvvv("debconf_in: {}".format(reply))
 
     def _thing_start(self, thing_type, thing):
         thing_name = " ".join(thing.get_name().strip().split())
         template = "run-ansible/progress/info/running"
 
-        conn = self._debconf_connection
-
-        if conn is not None:
-            conn.write("SUBST {} THINGTYPE {}\n".format(template, thing_type))
-            conn.write("SUBST {} THINGNAME {}\n".format(template, thing_name))
-            conn.write("PROGRESS INFO {}\n".format(template))
-            conn.flush()
+        self._communicate("SUBST {} THINGTYPE {}\n".format(template, thing_type))
+        self._communicate("SUBST {} THINGNAME {}\n".format(template, thing_name))
+        self._communicate("PROGRESS INFO {}\n".format(template))
 
     def v2_playbook_on_task_start(self, task, is_conditional):
         self._thing_start("task", task)
